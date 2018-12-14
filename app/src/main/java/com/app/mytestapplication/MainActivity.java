845fe4d4eb49c3411cc3315273c3855db7580b93
bujak.rafal@yahoo.com <bujak.rafal@yahoo.com>
2018-12-14 20:33:20 +0100

    create testing dialog in my app

--- app/src/main/java/com/app/mytestapplication/MainActivity.java
+++ app/src/main/java/com/app/mytestapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.app.mytestapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -22,7 +25,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         buttonClick = findViewById(R.id.buttonClick);
         buttonClick.setOnClickListener(this);
+
+        //createDialog();
+
+        createCustomeDialog();
     }
+
 
     @Override
     protected void onStop() {
@@ -40,5 +48,43 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (requestCode == 1 && resultCode == RESULT_OK) {
             Toast.makeText(this, data.getStringExtra("someData"), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void createDialog() {
+        Dialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Test")
+                .setMessage("You like this :)")
+                .setCancelable(false)
+                .setIcon(R.mipmap.ic_launcher)
+                .setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "What is not OK ", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }).show();
+    }
+
+
+    public void createCustomeDialog() {
+        final Dialog dialog = new Dialog(this);
+
+        dialog.setContentView(R.layout.dialog_test);
+        dialog.setTitle("test");
+
+
+        Button exit = dialog.findViewById(R.id.buttonExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
